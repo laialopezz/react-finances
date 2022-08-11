4aac32dfd0c031b5824d56ee3a1e2451e60a80ed
Laia <laialopezz99@gmail.com>
2022-08-11 22:50:53 +0200

    [ExpenseItem] useState usage

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,22 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate/ExpenseDate";
 import Card from "../../Shared/Card/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [name, setName] = useState(props.name);
+  const changeTitle = () => {
+    setName("Hello");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.name}</h2>
+        <h2>{name}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={changeTitle}>Change title</button>
     </Card>
   );
 };
